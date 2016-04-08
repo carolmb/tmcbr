265624cd6a1c0dcf88d60b1ccd148551de30da1c
Breno Viana <brenomauriciofv@gmail.com>
2016-04-08 10:57:24 -0300

    AI KnightArmor.

--- Assets/Scripts/Enemies/KnightArmor.cs
+++ Assets/Scripts/Enemies/KnightArmor.cs
@@ -1,19 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent (typeof(Character))]
 public class KnightArmor : MonoBehaviour {
 
+	// Character
 	private Character character;
+	// Max way
+	private int max_way;
+	// First position
+	private Vector3 firstPosition;
+	// First direction
+	private int firstDirection;
+	// Way
+	Queue<Vector2> way = new Queue<Vector2>();
 
+	//
 	void Awake() {
-		character = GetComponent<Character> ();
+		character = GetComponent<Character>();
+		max_way = 10;
+		// First tile
+		Vector2 position = transform.position;
+		firstDirection = character.direction;
+		// Check the direction
+		if (character.direction == 2) {
+			position.x += 1;
+		} else if (character.direction == 1) {
+			position.x -= 1;
+		} else if (character.direction == 0) {
+			position.y += 1;
+		}
+		way.Enqueue(Maze.WorldToTilePos(position));
+		firstPosition = transform.position;
 	}
 
+	//
 	void Update() {
-		// Checar o raio, depois o caminho, e então andar para o primeiro tile desse caminho
-		// Acessa o player por Player.instance
-		// Maze.WorldToTilePos()
+		CheckCollision();
 	}
 
+	// 
+	void CheckCollision() {
+		// First position
+		Vector2 position = way.Dequeue();
+		if (Maze.WorldToTilePos(Player.instance.transform.position) == Maze.WorldToTilePos(position)) {
+			// Wait one second
+			Invoke("Wait", 1);
+			// First movement
+			character.MoveTo(position);
+			// Try find the player
+			FindPlayer (position, max_way, false);
+		}
+	}
+
+	// Try find the player
+	void FindPlayer(Vector2 position, int count, bool founded) {
+		// If was founded
+		if (count == 0 && Maze.WorldToTilePos (Player.instance.transform.position) != position) {
+			founded = false;
+			ReturnToOriginalPosition ();
+		} else if (Maze.WorldToTilePos (Player.instance.transform.position) == position) {
+			founded = true;
+			// Wait one second
+			Invoke("Wait", 1);
+			Vector2 pos = way.Dequeue();
+			// Move
+			character.MoveTo(pos);
+		}
+		// Check the way
+		if (Maze.instance.tiles[(int) position.x, (int) position.y - 1].isWalkable == true && !founded) {
+			FindPlayer(new Vector2(position.x, position.y), --count, founded);
+		}
+		if (Maze.instance.tiles[(int) position.x, (int) position.y + 1].isWalkable == true && !founded) {
+			FindPlayer(new Vector2(position.x, position.y), --count, founded);
+		}
+		if (Maze.instance.tiles[(int) position.x - 1, (int) position.y].isWalkable == true && !founded) {
+			FindPlayer(new Vector2(position.x, position.y), --count, founded);
+		}
+		if (Maze.instance.tiles[(int) position.x + 1, (int) position.y].isWalkable == true && !founded) {
+			FindPlayer(new Vector2(position.x, position.y), --count, founded);
+		}
+	}
+
+	// 
+	void Attack() {
+		//
+	}
+
+	//
+	void ReturnToOriginalPosition() {
+		transform.position = firstPosition;
+		character.direction = firstDirection;
+	}
 }
