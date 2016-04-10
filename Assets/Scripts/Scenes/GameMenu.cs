18e6d595d55d47150125e6b370020fbdcef7241f
Luísa <luisamey@hotmail.com>
2016-04-09 22:25:19 -0300

    mais commit pq esqueci que add os bagulho

---
+++ Assets/Scripts/Scenes/GameMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour {
+
+	public Text saveName;
+	public Button[] saveButtons;
+
+	void Start() {
+		UpdateSaveButtons ();
+	}
+
+	public void UpdateSaveButtons() {
+		SaveManager.LoadSaves ();
+		for (int i = 0; i < SaveManager.maxSaves; i++) {
+			if (SaveManager.allSaves [i] != null) {
+				saveButtons [i].GetComponentInChildren<Text> ().text = SaveManager.allSaves [i].name;
+			} else {
+				saveButtons [i].GetComponentInChildren<Text> ().text = "Empty";
+			}
+		}
+	}
+
+	public void Save(int id) {
+		SaveManager.SaveGame (id, saveName.text);
+		UpdateSaveButtons ();
+	}
+
+	public void Quit() {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+}
