58645f865a80349cf8e452e649b11190ba6d5126
Breno Viana <brenomauriciofv@gmail.com>
2016-04-10 21:44:06 -0300

    Iniciando a implementação do movimento do KnightArmor.

--- Assets/Scripts/Enemies/KnightArmor.cs
+++ Assets/Scripts/Enemies/KnightArmor.cs
@@ -54,51 +54,56 @@ public class KnightArmor : MonoBehaviour {
 		if (character.moving == false) {
 			way.Clear ();
 			calcPosition ();
-		} else {
-			Debug.Log (character.moving);
 		}
 		// If the queue isn't empty
 		if (way.Count > 0) {
 			// First position
-			Vector2 position = way.Dequeue();
+			Vector2 position = way.Peek();
 			if (MazeManager.WorldToTilePos(Player.instance.transform.position) == MazeManager.WorldToTilePos(position)) {
 				// Wait one second
-				Invoke("Wait", 1);
+				StartCoroutine(Wait());
 				// First movement
 				StartCoroutine(character.MoveTo(position));
 				// Try find the player
 				FindPlayer (position, max_way, false);
 			}
-			way.Enqueue(position);
 		}
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(1);
 	}
 
 	// Try find the player
 	void FindPlayer(Vector2 position, int count, bool founded) {
+		Debug.Log (position);
 		// If was founded
-		if (count == 0 && MazeManager.WorldToTilePos (Player.instance.transform.position) != position) {
+		if (count == 0 && MazeManager.WorldToTilePos (Player.instance.transform.position) 
+			!= MazeManager.WorldToTilePos (position)) {
 			founded = false;
 			ReturnToOriginalPosition ();
-		} else if (MazeManager.WorldToTilePos (Player.instance.transform.position) == position) {
+		} else if (MazeManager.WorldToTilePos (Player.instance.transform.position) == MazeManager.WorldToTilePos (position)) {
 			founded = true;
-			// Wait one second
-			Invoke("Wait", 1);
+			Debug.Log ("Founded");
 			Vector2 pos = way.Dequeue();
 			// Move
-			character.MoveTo(pos);
+			StartCoroutine(character.MoveTo(MazeManager.WorldToTilePos (pos)));
 		}
-		// Check the way
-		if (MazeManager.maze.tiles[(int) position.x, (int) position.y - 1].isWalkable == true && !founded) {
-			FindPlayer(new Vector2(position.x, position.y), --count, founded);
-		}
-		if (MazeManager.maze.tiles[(int) position.x, (int) position.y + 1].isWalkable == true && !founded) {
-			FindPlayer(new Vector2(position.x, position.y), --count, founded);
-		}
-		if (MazeManager.maze.tiles[(int) position.x - 1, (int) position.y].isWalkable == true && !founded) {
-			FindPlayer(new Vector2(position.x, position.y), --count, founded);
-		}
-		if (MazeManager.maze.tiles[(int) position.x + 1, (int) position.y].isWalkable == true && !founded) {
-			FindPlayer(new Vector2(position.x, position.y), --count, founded);
+		// If the way isn't empty
+		if (way.Count > 0) {
+			// Check the way
+			if ((position.y > 1) && MazeManager.maze.tiles[(int) position.x, (int) (position.y - 1)].isWalkable == true && !founded) {
+				FindPlayer(new Vector2(position.x, position.y), --count, founded);
+			}
+			if (MazeManager.maze.tiles[(int) position.x, (int) position.y + 1].isWalkable == true && !founded) {
+				FindPlayer(new Vector2(position.x, position.y), --count, founded);
+			}
+			if ((position.x > 1) && MazeManager.maze.tiles[(int) (position.x - 1), (int) position.y].isWalkable == true && !founded) {
+				FindPlayer(new Vector2(position.x, position.y), --count, founded);
+			}
+			if (MazeManager.maze.tiles[(int) position.x + 1, (int) position.y].isWalkable == true && !founded) {
+				FindPlayer(new Vector2(position.x, position.y), --count, founded);
+			}
 		}
 	}
 
