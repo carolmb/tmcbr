a5746abec6ad7729287e2aceea44ad8e47bf3d7f
Ana Caroline <a_caroline96@hotmail.com>
2016-04-14 16:19:41 -0300

    raven

--- Assets/Scripts/StaticStage.cs
+++ Assets/Scripts/StaticStage.cs
@@ -7,32 +7,73 @@ public static class StaticStage {
 
 
 
-	static Maze LoadStaticStage (string nameStage, int id) {
-		Maze maze = new Maze (id, nameStage, 0, 0);
-		ReadFromFileBase (maze, nameStage);
-		ReadFromFileObjects (maze, nameStage);
+	static Maze LoadStaticStage (string stageName, int id) {
+		Maze maze = new Maze (id, stageName, 0, 0);
+		ReadFromFileBase (maze, stageName);
+		//ReadFromFileObjects (maze, stageName);
 		return maze;
 	}
 
 	//primeira linha tem width e height
 	//em seguida 0 para paredes, 1 para caminho livre
-	static void ReadFromFileBase (Maze maze, string nameFile) {
-		try
-		{   // Open the text file using a stream reader.
-			using (StreamReader sr = new StreamReader("base" + nameFile + ".txt"))
-			{
-				// Read the stream to a string, and write the string to the console.
-				string line = sr.ReadToEnd();
+	static void ReadFromFileBase (Maze maze, string fileName) {
+		try	{   
+			using (StreamReader sr = new StreamReader("base" + fileName + ".txt")) { //posteriormente colocar nome relativo do caminho "nomedafase/base.txt"
+				int width = sr.Read();
+				int height = sr.Read();
+				int value;
+				Tile[,] tiles = new Tile[width,height];
+				for (int i = 0; i < width; i++) {
+					for (int j = 0; j < height; j++) {
+						value = sr.Read();
+						tiles[i, j] = new Tile(i, j);
+						tiles[i, j].isWall = value == 0 ? false : true; 
+					}
+				}
 			}
 		}
-		catch (Exception e)
-		{
+		catch (Exception e) {
 			Debug.Log ("The file could not be read:");
 			Debug.Log (e.Message);
 		}
 	}
 
-	static void ReadFromFileObjects (Maze maze, string nameFile) {
-	
+	//nome do objeto seguido na proxima linha das coordenadas x e y
+	static void ReadFromFileObjects (Maze maze, string fileName) {
+		try { 
+			using (StreamReader sr = new StreamReader ("objects" + fileName + ".txt")) { // "nomedafase/objectos.txt"
+				string objectName;
+				int x, y;
+				while (!sr.EndOfStream) {
+					objectName = sr.ReadLine();
+					x = sr.Read();
+					y = sr.Read();
+					maze.tiles[x, y].objectName = objectName;
+				}
+
+			}
+		}
+		catch (Exception e)	{
+			Debug.Log ("The file could not be read:");
+			Debug.Log (e.Message);
+		}
+	}
+
+	static void ReadFromFileObstacles (Maze maze, string fileName) {
+		try { 
+			using (StreamReader sr = new StreamReader ("obstacles" + fileName + ".txt")) { // "nomedafase/objectos.txt"
+				int obstacle, x, y;
+				while (!sr.EndOfStream) {
+					obstacle = sr.Read();
+					x = sr.Read();
+					y = sr.Read();
+					maze.tiles[x, y].obstacle = obstacle;
+				}
+			}
+		}
+		catch (Exception e)	{
+			Debug.Log ("The file could not be read:");
+			Debug.Log (e.Message);
+		}
 	}
 }
