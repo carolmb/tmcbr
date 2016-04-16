e823c5b0652ba2800227d525186651e9fbead224
Breno Viana <brenomauriciofv@gmail.com>
2016-04-16 20:21:25 -0300

    Implementando o repelente para o Mimic.

--- Assets/Scripts/Enemies/Mimic.cs
+++ Assets/Scripts/Enemies/Mimic.cs
@@ -22,5 +22,17 @@ public class Mimic : Enemy {
 				character.MoveTo (nextPosition);
 			}
 		}
+		if (Player.instance.repel) {
+			GridPath path = PathToPlayer ();
+			if (path != null && path.PreviousSteps != null) {
+				Tile nextTile = path.PreviousSteps.LastStep;
+				Vector2 nextPosition = (Vector2)MazeManager.TileToWorldPosition (nextTile.coordinates) + new Vector2 (0, Tile.size / 2);
+				Vector2 np = new Vector2(nextPosition.x - transform.position.x, nextPosition.y - transform.position.y);
+				np.x *= -1;
+				np.y *= -1;
+				character.TurnTo (np);
+				character.MoveTo (np + (Vector2) transform.position);
+			}
+		}
 	}
 }
