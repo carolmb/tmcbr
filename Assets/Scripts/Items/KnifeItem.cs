f7767e51c7763eb7f9d6774481589f5afc5a0b12
Breno Viana <brenomauriciofv@gmail.com>
2016-04-19 22:23:38 -0300

    Animação da faca

--- Assets/Scripts/Items/KnifeItem.cs
+++ Assets/Scripts/Items/KnifeItem.cs
@@ -10,31 +10,32 @@ public class KnifeItem : MonoBehaviour {
 	// Velocidade
 	public float speed = 5;
 
-	// Tempo máximo até ser destruído
-	public float lifeTime = 1;
-
 	// Direção do movimento
 	private Vector2 moveVector;
+
+	// Tempo máximo até ser destruído
+	public float lifeTime = 2;
 
 	void Start () {
 		float posx = 0, posy = 0, posz = 0;
 		switch (Player.instance.character.direction) {
 			case 0:
-				transform.Rotate(0,0,-90);
+				transform.Rotate(0, 0,-120);
+				posy = Tile.size/4;
 				break;
 			case 1:
-				transform.Rotate (0, 0, 180);
-				posx = -(Tile.size/2);
+				transform.Rotate (0, 0, 150);
 				posy = Tile.size/2;
+				posx = -Tile.size/4;
 				break;
 			case 2:
-				transform.Rotate (0, 0, 0);
-				posx = Tile.size/2;
+				transform.Rotate (0, 0, -30);
+				posx = Tile.size/4;
 				posy = Tile.size/2;
 				break;
 			case 3:
-				transform.Rotate (0, 0, 90);
-				posy = Tile.size;
+				transform.Rotate (0, 0, 60);
+				posy = Tile.size/1.8f;
 				posz = 10;
 				break;
 		}
@@ -46,11 +47,31 @@ public class KnifeItem : MonoBehaviour {
 	void Update () {
 		// Atacar
 		Tile t = MazeManager.GetTile ((Vector2)transform.position - new Vector2 (0, Tile.size)); 
-		if (t == null || Player.instance.character.damaging) {
-			Destroy (gameObject);
-		} else {
-			lifeTime -= Time.deltaTime;
+		Quaternion target = Quaternion.Euler(0, 0, 0);
+		switch (Player.instance.character.direction) {
+		case 0:
+			Debug.Log("0");
+			target = Quaternion.Euler(0, 0, -30);
+			break;
+		case 1:
+			Debug.Log("1");
+			target = Quaternion.Euler(0, 0, 240);
+			break;
+		case 2:
+			Debug.Log("2");
+			target = Quaternion.Euler(0, 0, 30);
+			break;
+		case 3:
+			Debug.Log("3");
+			target = Quaternion.Euler(0, 0, 120);
+			break;
 		}
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 42);
+		Invoke ("DestroyKnife", 0.1f);
+	}
+
+	void DestroyKnife() {
+		Destroy (gameObject);
 	}
 
 	// Verifica se um inimigo foi atingido
