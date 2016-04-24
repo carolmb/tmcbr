da9e10df6600f7ad6dc689c0044de771bf267ac4
Breno Viana <brenomauriciofv@gmail.com>
2016-04-24 20:54:40 -0300

    Iniciando implementação do baú.

---
+++ Assets/Scripts/Items/Chest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+	// Moedas
+	private int coins;
+	private bool opened;
+
+	// Use this for initialization
+	void Start () {
+		opened = false;
+		coins = Random.Range (0, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//
+	}
+
+	// Abre o baú
+	void openChest() {
+		opened = true;
+		Player.instance.bag.coins += coins;
+	}
+}
