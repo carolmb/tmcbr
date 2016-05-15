dc355063382886e3d8a6dc0419915265a54e576e
Luísa <luisamey@hotmail.com>
2016-05-15 17:23:51 -0300

    DialogWindow

---
+++ Assets/Scripts/UI/DialogWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogWindow : MonoBehaviour {
+
+	public Image portrait;
+	public Text message;
+
+	void Start () {
+		portrait.gameObject.SetActive (false);
+		message.text = "";
+		gameObject.SetActive (false);
+	}
+
+	public void ShowDialog (string txt, string portraitName) {
+		gameObject.SetActive (true);
+		message.text = txt;
+		portrait.sprite = Resources.Load<Sprite> ("Images/Portraits/" + portraitName);
+	}
+
+	public void HideDialog() {
+		gameObject.SetActive (false);
+	}
+
+}
