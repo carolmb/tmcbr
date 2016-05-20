af74a87f914794478de0ca1b8c79ed17cf16d980
Ana Caroline <a_caroline96@hotmail.com>
2016-05-20 09:31:49 -0300

    inicio do golem1

---
+++ Assets/Scripts/Enemies/Golem1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Golem1 : Enemy {
+	
+	protected override void Start () {
+		base.Start ();
+	}
+
+	void Update () {
+		if (Player.instance.paused)
+			return;
+
+		if (!character.moving && !character.damaging) {
+			if (Player.instance.repelling) {
+				RunFromPlayer ();
+			} else {
+				ChasePlayer ();
+			}
+		}
+	}
+
+}
