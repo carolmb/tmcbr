93dea6dacd95fd1cd182a25da223d6e48a4542ac
Ana Caroline <a_caroline96@hotmail.com>
2016-05-21 21:10:41 -0300

    inicio do golem nivel 2, ainda desenvolvendo

---
+++ Assets/Scripts/Enemies/Golem2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Golem2 : Enemy {
+
+	protected override void Start () {
+		base.Start ();
+		Spawn ();
+	}
+
+	void Update () {
+		if (Player.instance.paused)
+			return;
+
+		if (!character.moving && !character.damaging) {
+			if (Player.instance.repelling) {
+				RunFromPlayer ();
+			} else {
+				ChasePlayer ();
+			}
+		}
+	}
+
+	void Spawn () {
+		if (!character.moving && !character.damaging) {
+			Tile t = ClosestToPlayer();
+			if (t != null) {
+				Invoke ("GolemRock", 3);
+			}
+		}
+		Invoke ("Spawn", 6);
+	}
+
+	void GolemRock() {
+		//Instantiate (golemrock, character.transform.position, character.transform.rotation);
+	}
+}
---
+++ Assets/Scripts/Enemies/GolemRock.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolemRock : MonoBehaviour {
+	// Dano causado ao inimigo
+	public int damage = 2;
+
+	// Velocidade
+	public float speed = 4;
+
+	// Direção do movimento
+	private Vector2 moveVector;
+
+	// Barulho ao colidir
+	public AudioClip collisionSound;
+
+	void Start() {
+		Vector3 pos;
+		pos.x = Player.instance.transform.position.x;
+		pos.z = Player.instance.transform.position.y;
+		pos.y = pos.z + 8;
+		transform.position = pos;
+		//moveVector = GameManager.AngleToVector (c.lookingAngle) * speed;
+	}
+
+	void Update () {
+		Tile t = MazeManager.GetTile ((Vector2)transform.position - new Vector2 (0, Tile.size)); 
+		if (t == null || t.isWall) {
+			Destroy (gameObject);
+		} else {
+			Vector3 pos = transform.position;
+			pos.x += moveVector.x * 60 * Time.deltaTime;
+			pos.y += moveVector.y * 60 * Time.deltaTime;
+			pos.z += moveVector.y * 60 * Time.deltaTime;
+			transform.position = pos;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.CompareTag ("Player")) {
+			Character comp = collider.GetComponent<Character> ();
+			comp.Damage ((Vector2) transform.position - moveVector * 10, damage);
+			//GameCamera.PlayAudioClip (collisionSound);
+			Destroy (gameObject);
+		}
+	}
+}
