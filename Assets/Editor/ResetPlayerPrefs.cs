f312bd2adfbe4cd7d81f452d90eb7464c370814c
Luísa <luisamey@hotmail.com>
2016-05-28 07:17:04 -0300

    uns bug louco com material

---
+++ Assets/Editor/ResetPlayerPrefs.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ResetPlayerPrefs {
+
+	[MenuItem("Edit/Reset Playerprefs")] public static void DeletePlayerPrefs() { PlayerPrefs.DeleteAll(); }
+
+}
